6e210c31366eccc45582e6d058dc9a1d86e1d4a3
amaechijude <ugwuamaechi5@gmail.com>
2024-05-15 18:08:11 +0100

    week 3

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,34 +1,36 @@
-﻿static string lessThan100(int num1, int num2) {
-    int result = num1 + num2;
-    if (result < 100) {
-        return $"The sum of {num1} and {num2} is less than 100";
-    } else {
-        return $"The sum of {num1} and {num2} is greater than 100";
-    }
-}
-
-static string primeCheck(int num) {
+﻿static string primeCheck(int num) {
     // base case a: numbers less that 1 are not prime
     if (num <= 1) {
-        return $"{num} is not a prime";
+        return $"{num} is not a prime number";
     }
     // base case b: 2 and 3 are prime numbers
     if (num <= 3) {
-        return $"{num} is a prime";
+        return $"{num} is a prime number";
     }
-    // square root of the number which is in double
-    double numRoot = Math.Sqrt(num);
-    int numRound = (int)numRoot;
+    // square root of the number
+    int numRound = (int)Math.Sqrt(num);
     // iterate from 2 to the square root of the number
     for (int i = 2; i <= numRound + 1; i++)
      {
         //  Check if any of the modules is zero
         if (num % i == 0) {
-            return $"{num} is not a prime"; 
+            return $"{num} is not a prime number"; 
         }
     }
-    return $"{num} is a prime";
+    return $"{num} is a prime number";
 }
+
+
+static string lessThan100(int num1, int num2) {
+    int result = num1 + num2;
+    if (result < 100) {
+        return $"true // {num1} + {num2} = {result}";
+    } else {
+        return $"false // {num1} + {num2} = {result}";
+    }
+}
+
 //test
+
 Console.WriteLine(primeCheck(17));
-Console.WriteLine(lessThan100(22,15));
+Console.WriteLine(lessThan100(83, 34));
---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to Remainder checker. Press Enter to continue ");
+Console.ReadLine();
+
+static string Remainder(int x, int y) {
+    int result = x % y;
+
+    return $"The remainder is {result}";
+}
+
+Console.WriteLine("Enter the first number ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+var output = Remainder(x, y);
+
+Console.WriteLine(output);
