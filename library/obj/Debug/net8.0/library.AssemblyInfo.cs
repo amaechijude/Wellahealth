b1480009c7904f3dae8d778134966a9d488a0392
amaechijude <ugwuamaechi5@gmail.com>
2024-07-16 14:48:41 +0100

    mint

--- library/obj/Debug/net8.0/library.AssemblyInfo.cs
+++ library/obj/Debug/net8.0/library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23c50100d820050385d5e781137e271aa9bcaea5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3639e125f98b32321d2ddf0b516482fde74408cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
