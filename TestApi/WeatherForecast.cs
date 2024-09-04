d98ec23b4d94008ce829188ea5ded12f8e4a99cf
amaechijude <ugwuamaechi5@gmail.com>
2024-09-04 11:54:22 +0100

    test api

---
+++ TestApi/WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace TestApi;
+
+public class Haha
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+    
+    public string? Greeting { get; set; }
+}
